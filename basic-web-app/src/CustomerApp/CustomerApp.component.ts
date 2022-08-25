90c2fd07c10768539059b0fc8a86a31d44b6ca30
Varun Agrawal <vrnvav97@rediffmail.com>
2022-08-25 22:26:18 +0530

    Added Table

--- basic-web-app/src/CustomerApp/CustomerApp.component.ts
+++ basic-web-app/src/CustomerApp/CustomerApp.component.ts
@@ -10,4 +10,9 @@ import { Customer } from './CustomerApp.model';
 export class AppComponent {
   title = 'Customer Application';
   CustomerModel:Customer = new Customer();
+  CustomerModels:Array<Customer> = new Array<Customer>();
+  Add(){
+    this.CustomerModels.push(this.CustomerModel);
+    this.CustomerModel=new Customer();
+  }
 }
